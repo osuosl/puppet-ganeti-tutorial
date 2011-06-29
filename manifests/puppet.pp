36ac582ad70619e379889a43a1b7b55feec7a046
Lance Albertson <lance@osuosl.org>
2011-06-28 23:23:36 -0700

    fix typoes

--- manifests/puppet.pp
+++ manifests/puppet.pp
@@ -2,11 +2,11 @@ class ganeti_tutorial::puppet {
     $deb_dir = "/etc/puppet/modules/ganeti_tutorial/files/deb"
     package {
         "puppet-common":
-            ensure      => latest;
+            ensure      => latest,
             provider    => dpkg,
             source      => "${deb_dir}/puppet-common_2.7.0~rc4-1_all.deb";
         "puppet":
-            ensure      => latest;
+            ensure      => latest,
             provider    => dpkg,
             source      => "${deb_dir}/puppet_2.7.0~rc4-1_all.deb";
     }
