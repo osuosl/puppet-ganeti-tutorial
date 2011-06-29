7b3ecf20809da0a8466cb63dc5a4e53a4a948df8
Lance Albertson <lance@osuosl.org>
2011-06-29 00:29:46 -0700

    sync naming standard

--- manifests/gwm.pp
+++ manifests/gwm.pp
@@ -17,7 +17,7 @@ class ganeti_tutorial::gwm {
     file { "/var/lib/django": ensure => directory }
 
     exec { 
-        "unpack_gwm":
+        "unpack-gwm":
             command => "/bin/tar -zxf /root/src/ganeti-webmgr.0.7.2.tar.gz",
             cwd     => "/var/lib/django",
             creates => "/var/lib/django/ganeti_webmgr",
@@ -27,6 +27,6 @@ class ganeti_tutorial::gwm {
             cwd     => "/var/lib/django/ganeti_webmgr",
             creates => "/var/lib/django/ganeti_webmgr/bin/activate",
             require => [ Package["fabric"], Package["virtualenv"], 
-                        Package["python-dev"], Exec["unpack_gwm"] ];
+                        Package["python-dev"], Exec["unpack-gwm"] ];
     }
 }
