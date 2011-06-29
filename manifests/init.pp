5c020f90eb890556390db84dd06b6907e57ad134
Lance Albertson <lance@osuosl.org>
2011-06-28 19:54:55 -0700

    ensure .ssh exists

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,4 +4,6 @@ class ganeti_tutorial {
     include ganeti_tutorial::install_deps
     include ganeti_tutorial::hosts
     include ganeti_tutorial::networking
+
+    file { "/root/.ssh": ensure => directory, }
 }
