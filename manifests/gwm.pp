a6ec39d0d289cd9de79bc19641352c5aa185b2d8
Lance Albertson <lance@osuosl.org>
2011-06-28 22:46:16 -0700

    Install, deploy gwm

---
+++ manifests/gwm.pp
@@ -0,0 +1,22 @@
+class ganeti_tutorial::gwm {
+    package {
+        "python-pip":   ensure => "installed";
+        "fabric":
+            ensure      => "installed",
+            require     => Package["python-pip"],
+            provider    => "installed";
+    }
+
+    file { "/var/lib/django": ensure => directory }
+
+    exec { 
+        "unpack_gwm":
+            command => "tar -zxf /root/src/ganeti-webmgr.0.7.2.tar.gz",
+            cwd     => "/var/lib/django",
+            require => [ File["/root/src"], File["/var/lib/django"] ];
+        "deploy-gwm":
+            command => "fab prod deploy",
+            cwd     => "/var/lib/django/ganeti_webmgr",
+            require => [ Package["fabric"], Exec["unpack_gwm"] ];
+    }
+}
