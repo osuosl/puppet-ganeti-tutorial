528e4341109aee6523c0f93a8702dfef698e8e9d
Lance Albertson <lance@osuosl.org>
2011-06-28 20:02:06 -0700

    add symlinks to kernel

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,5 +5,14 @@ class ganeti_tutorial {
     include ganeti_tutorial::hosts
     include ganeti_tutorial::networking
 
-    file { "/root/.ssh": ensure => directory, }
+    file {
+        "/root/.ssh":
+            ensure  => directory;
+        "/boot/vmlinuz-2.6-kvmU":
+            ensure  => link,
+            target  => "/boot/vmlinuz-2.6.32-5-amd64";
+        "/boot/initrd-2.6-kvmU":
+            ensure  => link,
+            target  => "/boot/initrd-2.6.32-5-amd64";
+    }
 }
