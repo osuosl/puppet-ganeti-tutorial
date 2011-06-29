f038ba4c8ef46107a67d80e0d9cce156974cd7cf
Lance Albertson <lance@osuosl.org>
2011-06-28 23:56:17 -0700

    fix formatting

--- manifests/install_deps.pp
+++ manifests/install_deps.pp
@@ -1,26 +1,26 @@
 class ganeti_tutorial::install_deps {
     package {
         # Ganeti deps
-        "lvm2":             ensure=> installed;
-        "bridge-utils":     ensure=> installed;
-        "iproute":          ensure=> installed;
-        "iputils-arping":   ensure=> installed;
-        "ndisc6":           ensure=> installed;
-        "python-openssl":   ensure=> installed;
-        "openssl":          ensure=> installed;
-        "python-pyparsing": ensure=> installed;
-        "python-simplejson": ensure=> installed;
-        "python-pyinotify": ensure=> installed;
-        "python-pycurl":    ensure=> installed;
-        "socat":            ensure=> installed;
-        "python-paramiko":  ensure=> installed;
+        "lvm2":             ensure => installed;
+        "bridge-utils":     ensure => installed;
+        "iproute":          ensure => installed;
+        "iputils-arping":   ensure => installed;
+        "ndisc6":           ensure => installed;
+        "python-openssl":   ensure => installed;
+        "openssl":          ensure => installed;
+        "python-pyparsing": ensure => installed;
+        "python-simplejson": ensure => installed;
+        "python-pyinotify": ensure => installed;
+        "python-pycurl":    ensure => installed;
+        "socat":            ensure => installed;
+        "python-paramiko":  ensure => installed;
         # Instance Image
-        "qemu-utils":       ensure=> installed;
-        "dump":             ensure=> installed;
-        "kpartx":           ensure=> installed;
-        "kvm":              ensure=> installed;
+        "qemu-utils":       ensure => installed;
+        "dump":             ensure => installed;
+        "kpartx":           ensure => installed;
+        "kvm":              ensure => installed;
         # Misc
-        "vim":              ensure=> installed;
+        "vim":              ensure => installed;
     }
 
     file {
