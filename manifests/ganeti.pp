4515cec9772d3ef8566448e14ba31bcc046f4deb
Lance Albertson <lance@osuosl.org>
2011-07-01 00:21:10 -0700

    fix typo

--- manifests/ganeti.pp
+++ manifests/ganeti.pp
@@ -37,7 +37,7 @@ class ganeti_tutorial::ganeti::install {
 class ganeti_tutorial::ganeti::initialize inherits ganeti_tutorial::ganeti::install {
     exec {
         "initialize-ganeti":
-            command => "/usr/local/sbin/gnt-cluster init --vg-name=ganeti -s 192.168.16.16 --master-netdev=br0 -H kvm:kernel_path=/boot/vmlinuz-2.6=kvmU,initrd_path=/boot/initrd.img-2.6-kvmU,nic_type=e1000,disk_type=scsi,vnc_bind_address=0.0.0.0,serial_console=true --enabled-hypervisors=kvm ganeti.example.org",
+            command => "/usr/local/sbin/gnt-cluster init --vg-name=ganeti -s 192.168.16.16 --master-netdev=br0 -H kvm:kernel_path=/boot/vmlinuz-2.6=kvmU,initrd_path=/boot/initrd-img-2.6-kvmU,nic_type=e1000,disk_type=scsi,vnc_bind_address=0.0.0.0,serial_console=true --enabled-hypervisors=kvm ganeti.example.org",
             creates => "/var/lib/ganeti/config.data",
             require => Exec["install-ganeti"],
     }
