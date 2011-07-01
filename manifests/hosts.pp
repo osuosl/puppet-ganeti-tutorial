5c1a8874ec85499542387feb508224f84d9b6c8d
Lance Albertson <lance@osuosl.org>
2011-06-30 22:26:43 -0700

    Adjust networking to use 3 nics
    The NAT network on VirtualBox makes it virtual impossible to have two VMs
    talking to each other. So instead of fighting that we'll have two internal
    networks, one for the nodes, another for drbd traffic.

--- manifests/hosts.pp
+++ manifests/hosts.pp
@@ -2,18 +2,18 @@ class ganeti_tutorial::hosts {
     host {
         "ganeti.example.org":
             ensure          => present,
-            ip              => "10.0.2.15";
+            ip              => "10.1.0.15";
         "node1.example.org":
             ensure          => present,
-            ip              => "10.0.2.16";
+            ip              => "10.1.0.16";
         "node2.example.org":
             ensure          => present,
-            ip              => "10.0.2.17";
+            ip              => "10.1.0.17";
         "instance1.example.org":
             ensure          => present,
-            ip              => "10.0.2.30";
+            ip              => "10.1.0.30";
         "instance2.example.org":
             ensure          => present,
-            ip              => "10.0.2.30";
+            ip              => "10.1.0.30";
     }
 }
--- nodes/node1.pp
+++ nodes/node1.pp
@@ -1,4 +1,4 @@
-$host_ip = "10.0.2.15"
+$host_ip = "10.1.0.16"
 $drbd_ip = "192.168.16.15"
 
 include ganeti_tutorial
