0759b81c64177ead3408bf5817fe024a5def82a1
Lance Albertson <lance@osuosl.org>
2011-07-03 23:34:54 -0700

    Add host aliases, fix ip

--- manifests/hosts.pp
+++ manifests/hosts.pp
@@ -2,18 +2,23 @@ class ganeti_tutorial::hosts {
     host {
         "ganeti.example.org":
             ensure          => present,
+            host_aliases    => "ganeti",
             ip              => "10.1.0.15";
         "node1.example.org":
             ensure          => present,
+            host_aliases    => "node1",
             ip              => "10.1.0.16";
         "node2.example.org":
             ensure          => present,
+            host_aliases    => "node2",
             ip              => "10.1.0.17";
         "instance1.example.org":
             ensure          => present,
+            host_aliases    => "instance1",
             ip              => "10.1.0.30";
         "instance2.example.org":
             ensure          => present,
-            ip              => "10.1.0.30";
+            host_aliases    => "instance2",
+            ip              => "10.1.0.31";
     }
 }
