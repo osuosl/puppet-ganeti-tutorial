c63c8d146c9e8ee5a78c8d5243a3d4767a97e446
Lance Albertson <lance@osuosl.org>
2011-07-03 23:31:13 -0700

    We don't initialize node2

--- nodes/node2/initialize-ganeti.pp
+++
@@ -1,7 +0,0 @@
-$host_ip = "10.1.0.17"
-$drbd_ip = "192.168.16.17"
-
-include ganeti_tutorial
-include ganeti_tutorial::instance_image
-include ganeti_tutorial::ganeti::install
-include ganeti_tutorial::ganeti::initialize
