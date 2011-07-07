ec6b5714a1229935fd0733dea46e24203efa2b42
Lance Albertson <lance@osuosl.org>
2011-07-07 08:45:45 -0700

    Set proper kernel path based on arch

--- manifests/kvm.pp
+++ manifests/kvm.pp
@@ -1,4 +1,6 @@
 class ganeti_tutorial::kvm {
+    $kern_version = "2.6.32-5"
+
     package {
         "kvm":  ensure => installed;
     }
@@ -6,10 +8,16 @@ class ganeti_tutorial::kvm {
     file {
         "/boot/vmlinuz-2.6-kvmU":
             ensure  => link,
-            target  => "/boot/vmlinuz-2.6.32-5-amd64";
+            target  => $architecture ? {
+                x86_64  => "/boot/vmlinuz-${kern_version}-amd64",
+                i386    => "/boot/vmlinuz-${kern_version}-686",
+            };
         "/boot/initrd-2.6-kvmU":
             ensure  => link,
-            target  => "/boot/initrd.img-2.6.32-5-amd64";
+            target  => $achitecture ? {
+                x86_64  => "/boot/initrd.img-${kern_version}-amd64",
+                i386    => "/boot/initrd.img-${kern_version}-686",
+            };
     }
 
     service {
