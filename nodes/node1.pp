03ac5b59be4cb292ef951449371c8d613cf931cd
Lance Albertson <lance@osuosl.org>
2012-05-08 09:08:02 -0700

    Update ganeti-only puppet manifest

--- nodes/node1.pp
+++ nodes/node1.pp
@@ -2,7 +2,11 @@ $host_ip = "33.33.33.11"
 $drbd_ip = "33.33.34.11"
 
 include ganeti_tutorial
+include ganeti_tutorial::networking
 include ganeti_tutorial::kvm
 include ganeti_tutorial::instance_image
-include ganeti_tutorial::htools
+#include ganeti_tutorial::htools
 include ganeti_tutorial::ganeti::install
+include ganeti_tutorial::ganeti::initialize
+
+File { owner => "root", group => "root", }
